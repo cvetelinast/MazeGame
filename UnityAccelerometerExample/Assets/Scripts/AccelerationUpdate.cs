785c8c334acdc2f3d122b55cb99a7ba2b09db6f6
Cvetelina Stoyanova <cvetelinast.96@abv.bg>
2019-12-29 15:44:16 +0200

    Added maze generation and sensitivity of acceleration

---
+++ UnityAccelerometerExample/Assets/Scripts/AccelerationUpdate.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AccelerationUpdate : MonoBehaviour
+{
+    public Text acceleration = null;
+
+    // Update is called once per frame
+    void Update()
+    {
+        acceleration.text = "Acceleration: " + Input.acceleration;
+    }
+}
---
+++ UnityAccelerometerExample/Assets/Scripts/Accelerometer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Accelerometer : MonoBehaviour
+{
+    private Rigidbody rigid;
+    private float speedCoeff = 1;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    public void AdjustSpeed(float coeff)
+    {
+        speedCoeff = coeff;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        Vector3 acc = Input.acceleration;
+        acc = Quaternion.Euler(90, 0, 0) * acc;
+        var force = acc * speedCoeff * Time.deltaTime;
+        Debug.Log("Force: " + force + ", acc: " + acc + ", Input.acceleration: " + Input.acceleration + ", speedCoeff: " + speedCoeff + " , deltaTime: " + Time.deltaTime);
+        rigid.AddForce(force);
+    }
+}
